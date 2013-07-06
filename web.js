0f7d1e3ad83d9e1163f08361e44938850bd30192
Prajwal Y <prajwal299@gmail.com>
2013-07-06 06:49:10 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(Buffer.toString(fs.readFile(index.html)));
+  response.send(Buffer.toString(fs.readFile('index.html')));
 });
 
 var port = process.env.PORT || 5000;
